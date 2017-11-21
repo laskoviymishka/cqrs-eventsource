79e58d858e774ec26fbd53f083a08231dc486f06
Andrei_Tserakhau <and>
2017-11-21 13:56:56 +0300

    feat: add basic bus implementation

---
+++ eventsource/services/commandbus.py
@@ -0,0 +1,119 @@
+import inspect
+
+from abc import abstractmethod, ABCMeta
+from typing import Generic, TypeVar, Dict, Callable
+
+TInput = TypeVar('TInput')
+TOutput = TypeVar('TOutput')
+
+
+class Command(metaclass=ABCMeta):
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+
+
+class AggregateCommand(Command, metaclass=ABCMeta):
+    def __init__(self, originator_id, **kwargs):
+        self.originator_id = originator_id
+        super().__init__(**kwargs)
+
+
+class CommandHandler(metaclass=ABCMeta):
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+
+    @abstractmethod
+    async def handle(self, command: Command) -> None: ...
+
+
+class FunctionBaseHandler(CommandHandler):
+    def __init__(self, func: Callable, **kwargs):
+        self.func = func
+        super().__init__(**kwargs)
+
+    async def handle(self, command: Command):
+        await self.func(command, **self.__dict__)
+
+
+class QuerySpec(Generic[TInput, TOutput], metaclass=ABCMeta):
+    def __init__(self, data: TInput):
+        self.data: TInput = data
+
+
+class QueryProcessor(Generic[TInput, TOutput], metaclass=ABCMeta):
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+
+    @abstractmethod
+    async def run(self, query: QuerySpec[TInput, TOutput]) -> TOutput: ...
+
+
+class FunctionBaseProcessor(QueryProcessor[TInput, TOutput]):
+    def __init__(self, func: Callable, **kwargs):
+        self.func = func
+        super().__init__(**kwargs)
+
+    async def run(self, query: QuerySpec[TInput, TOutput]):
+        return await self.func(query, **self.__dict__)
+
+
+class BusResolverError(BaseException): ...
+
+
+_QUERY_HANDLERS: Dict[type, Callable] = {}
+_COMMAND_HANDLERS: Dict[type, Callable] = {}
+
+
+def command_handler(command_cls: type):
+    def decorator(func):
+        if inspect.isclass(func):
+            _COMMAND_HANDLERS[command_cls] = func
+        elif inspect.isfunction(func):
+            def factory(**kwargs):
+                return FunctionBaseHandler(func, **kwargs)
+
+            _COMMAND_HANDLERS[command_cls] = factory
+        else:
+            print('command handler must be a function or class')
+            raise BusResolverError
+
+        return func
+
+    return decorator
+
+
+def query_processor(query_cls: type):
+    def decorator(func):
+        if inspect.isclass(func):
+            _QUERY_HANDLERS[query_cls] = func
+        elif inspect.isfunction(func):
+            def factory(**kwargs):
+                return FunctionBaseProcessor(func, **kwargs)
+
+            _QUERY_HANDLERS[query_cls] = factory
+        else:
+            print('query processor must be a function or class')
+            raise BusResolverError
+
+        return func
+
+    return decorator
+
+
+class Bus(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+
+    async def run_query(self, query: QuerySpec) -> TOutput:
+        if type(query) not in _QUERY_HANDLERS.keys():
+            raise BusResolverError
+
+        handler = _QUERY_HANDLERS[type(query)](**self.__dict__)
+        return await handler.run(query)
+
+    async def execute_command(self, command: Command) -> None:
+        if type(command) not in _COMMAND_HANDLERS.keys():
+            raise BusResolverError
+
+        handler = _COMMAND_HANDLERS[type(command)](**self.__dict__)
+        await handler.handle(command)
---
+++ tests/bus_tests.py
@@ -0,0 +1,139 @@
+import unittest
+
+from aiounittest import async_test
+
+from eventsource.services.commandbus import Command, command_handler, Bus, QuerySpec, query_processor, BusResolverError, \
+    AggregateCommand
+
+
+class BusTests(unittest.TestCase):
+    @async_test
+    async def test_command_handler_should_called(self):
+        was_called = False
+
+        class TestCommand(Command): ...
+
+        @command_handler(TestCommand)
+        async def test_command_function(command: TestCommand, **kwargs):
+            nonlocal was_called
+            self.assertIsNotNone(command)
+            was_called = True
+
+        bus = Bus()
+        await bus.execute_command(TestCommand())
+        self.assertTrue(was_called)
+
+    @async_test
+    async def test_command_should_pass_argument_from_bus_constructor(self):
+        was_called = False
+
+        class TestCommand(Command): ...
+
+        @command_handler(TestCommand)
+        async def test_command_function(command: TestCommand, test_data: bool, **kwargs):
+            nonlocal was_called
+            was_called = test_data
+
+        bus = Bus(test_data=True)
+        await bus.execute_command(TestCommand())
+        self.assertTrue(was_called)
+
+    @async_test
+    async def test_command_should_raise_argument_error_when_bus_not_have_one(self):
+        class TestCommand(Command):
+            ...
+
+        @command_handler(TestCommand)
+        async def test_command_function(query: TestCommand, not_exist_data: bool, **kwargs):
+            ...
+
+        error_raised = False
+        bus = Bus(test_data=True)
+        try:
+            await bus.execute_command(TestCommand())
+        except TypeError:
+            error_raised = True
+
+        self.assertTrue(error_raised)
+
+    @async_test
+    async def test_query_handler_should_return(self):
+        class TestQuery(QuerySpec[bool, bool]): ...
+
+        @query_processor(TestQuery)
+        async def test_query_processor(query: TestQuery, **kwargs) -> bool:
+            self.assertTrue(query.data)
+            return True
+
+        bus = Bus()
+        res = await bus.run_query(TestQuery(True))
+        self.assertTrue(res)
+
+    @async_test
+    async def test_query_should_pass_argument_from_bus_constructor(self):
+        class TestQuery(QuerySpec[bool, bool]): ...
+
+        @query_processor(TestQuery)
+        async def test_query_processor(query: TestQuery, test_data: bool, **kwargs) -> bool:
+            return test_data
+
+        bus = Bus(test_data=True)
+        self.assertTrue(await bus.run_query(TestQuery(True)))
+
+    @async_test
+    async def test_query_should_raise_argument_error_when_bus_not_have_one(self):
+        class TestQuery(QuerySpec[bool, bool]):
+            ...
+
+        @query_processor(TestQuery)
+        async def test_query_processor(query: TestQuery, not_exist_data: bool, **kwargs) -> bool:
+            return True
+
+        error_raised = False
+        bus = Bus(test_data=True)
+        try:
+            await bus.run_query(TestQuery(True))
+        except TypeError:
+            error_raised = True
+
+        self.assertTrue(error_raised)
+
+    async def test_command_should_raise_when_command_not_handled(self):
+        class TestCommand(Command):
+            ...
+
+        error_raised = False
+        bus = Bus()
+        try:
+            bus.execute_command(TestCommand())
+        except BusResolverError:
+            error_raised = True
+
+        self.assertTrue(error_raised)
+
+    async def test_query_should_raise_when_command_not_handled(self):
+        class TestQuery(QuerySpec[bool, bool]):
+            ...
+
+        error_raised = False
+        bus = Bus()
+        try:
+            bus.run_query(TestQuery(True))
+        except BusResolverError:
+            error_raised = True
+
+        self.assertTrue(error_raised)
+
+    async def test_aggregate_command_should_pass_originator(self):
+        class TestAggregateCommand(AggregateCommand): ...
+
+        passed_id = -1
+        expected_id = 123
+
+        async def test_handler(command: TestAggregateCommand, **kwargs):
+            nonlocal passed_id
+            passed_id = command.originator_id
+
+        bus = Bus()
+        await bus.execute_command(TestAggregateCommand(originator_id=expected_id))
+        self.assertEqual(expected_id, passed_id)
